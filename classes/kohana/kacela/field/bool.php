76b4cf4ef79fea3bb79053eb6d2c9186b7eb6ee8
Ben Midget <benmidget@gmail.com>
2012-10-07 14:39:20 -0600

    correct transform logic

--- classes/kohana/kacela/field/bool.php
+++ classes/kohana/kacela/field/bool.php
@@ -5,4 +5,15 @@ use Gacela\Field as F;
 class Kohana_Kacela_Field_Bool extends F\Bool
 {
 
+public static function transform($meta, $value, $in = true)
+{
+	if($in && is_bool($value)) {
+		$value === true ? $value = 1 : $value = 0;
+	} elseif(!$in) {
+		$value = (bool) $value;
+	}
+
+	return $value;
+}
+
 }
